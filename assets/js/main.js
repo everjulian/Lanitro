6d3adef91343c797e07ff919e6fdb001d0806093
coff3programing <coff3programing@gmail.com>
2023-08-16 17:17:22 -0500

    he quitado el registrar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import countdown from "./dom/countdown.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
-  countdown("countdown", "August 31, 2023 07:00:00", "Fin de la promo😁");
+  countdown("countdown", "August 26, 2023 07:00:00", "Fin de la promo😁");
 });
